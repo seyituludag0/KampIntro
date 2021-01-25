8004e049a7ddd9df362990690c64415cfd39f39f
Seyit Uludağ <seyituludag0@outlook.com>
2021-01-25 16:35:01 +0300

    25 Ocak 16:34

---
+++ Interfaces2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Add();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new EmployeeManager());
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager:IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Customer Added");
+        }
+    }
+    class EmployeeManager: IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Employee Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Employee Added");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
